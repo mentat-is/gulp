2086f5d5decb67add0959ebf2060151fc8003760
valerino <xoanino@gmail.com>
2024-10-04 09:51:36 +0200

    run script to freeze requirements during push on master branch / tag / manual trigger in docker-ci.yml

--- update_requirements_txt.sh
+++ update_requirements_txt.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # freeze requirements to requirements.txt
+echo [.] Freezing requirements to requirements.txt
 pip3 freeze >/tmp/requirements.txt
+
+echo [.] Removing gulp from freezed requirements.txt
 sed '/gulp@/d' /tmp/requirements.txt >/tmp/requirements2.txt
 sed 's/^-e //g' /tmp/requirements2.txt >./requirements.txt
 cat ./requirements.txt
