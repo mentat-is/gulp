495db5512f72107ce3559060620005b423ec3e87
valerino <xoanino@gmail.com>
2024-10-04 10:02:20 +0200

    reverted previous commit: it is better to add requirements.txt manually when needed (push on master or tag), then running the CI there will find the requirements.txt and use it

--- update_requirements_txt.sh
+++ update_requirements_txt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# freeze requirements to requirements.txt
+# this should be run when pushing to master branch or stable tag to freeze requirements to requirements.txt
 echo [.] Freezing requirements to requirements.txt
 pip3 freeze >/tmp/requirements.txt
 
@@ -7,3 +7,5 @@ echo [.] Removing gulp from freezed requirements.txt
 sed '/gulp@/d' /tmp/requirements.txt >/tmp/requirements2.txt
 sed 's/^-e //g' /tmp/requirements2.txt >./requirements.txt
 cat ./requirements.txt
+
+echo [.] Done, now add requirements.txt, commit and force push to master branch or stable tag.
