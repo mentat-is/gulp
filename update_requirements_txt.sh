fa0a83bdb619ec89ffdab418b7fa5d37b33d56b8
valerino <xoanino@gmail.com>
2025-05-28 07:41:52 +0000

    fixed update_requirements_txt.sh

--- update_requirements_txt.sh
+++ update_requirements_txt.sh
@@ -4,7 +4,7 @@ echo "[.] Updating requirements.txt"
 
 # Get pip freeze output, remove -e lines, and add muty package
 pip3 freeze | grep -v "^-e" > requirements.txt
-#echo "muty@git+https://github.com/mentat-is/muty-python.git@develop" >> requirements.txt
+echo "muty@git+https://github.com/mentat-is/muty-python.git@develop" >> requirements.txt
 
 echo "[.] Done. Updated requirements.txt:"
 echo "-----------------------------------"
