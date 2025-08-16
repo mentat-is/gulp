1c73c636ce29352bac591761cc6cb4466c54ebba
valerino <xoanino@gmail.com>
2025-08-16 08:48:32 +0000

    test_scripts: directory cleanup from outdated code

--- test_scripts/set_extra_path.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export PATH_MAPPING_FILES_EXTRA=/gulp/gulp-paid-plugins/src/gulp-paid-plugins/mapping_files
-export PATH_PLUGINS_EXTRA=/gulp/gulp-paid-plugins/src/gulp-paid-plugins/plugins
-echo "PATH_MAPPING_FILES_EXTRA=$PATH_MAPPING_FILES_EXTRA"
-echo "PATH_PLUGINS_EXTRA=$PATH_PLUGINS_EXTRA"
-echo "extra paths set!"
